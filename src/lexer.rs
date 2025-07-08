78485e9fb3ea5502d424b8c9c26b2e1ab8090ab9
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-07-08 12:19:31 +0700

    Implement lexer

---
+++ src/lexer.rs
@@ -0,0 +1,167 @@
+use crate::{Error, SourcePosition};
+use std::{collections::HashMap, ops::Range};
+
+pub struct Lexer<'a> {
+    input: &'a str,
+    position: SourcePosition,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            input,
+            position: SourcePosition {
+                line: 1,
+                column: 1,
+                offset: 0,
+            },
+        }
+    }
+
+    pub fn next_token(&mut self) -> Result<Token, Error> {
+        self.skip_whitespace();
+        let start_position = self.position.clone();
+
+        let ch = match self.next_char() {
+            Some(c) => c,
+            None => {
+                return Ok(Token {
+                    kind: TokenKind::EOF,
+                    span: start_position.until(&self.position),
+                });
+            }
+        };
+
+        let kind = match ch {
+            '0' => {
+                if !self.is_at_word_boundary() {
+                    return Err(Error {
+                        message: "Non-decimal integer constants not supported".to_string(),
+                        span: start_position.until(&self.position),
+                    });
+                }
+                TokenKind::IntConstant(0)
+            }
+            '1'..='9' => {
+                let mut number = ch.to_string();
+                while let Some(next_ch) = self.peek_char() {
+                    if next_ch.is_digit(10) {
+                        number.push(self.next_char().unwrap());
+                    } else if !self.is_at_word_boundary() {
+                        let invalid_ch = self.next_char().unwrap();
+                        return Err(Error {
+                            message: format!("Invalid digit '{}' in decimal constant", invalid_ch),
+                            span: start_position.until(&self.position),
+                        });
+                    } else {
+                        break;
+                    }
+                }
+
+                TokenKind::IntConstant(number.parse::<i64>().unwrap())
+            }
+            'a'..='z' | 'A'..='Z' | '_' => {
+                let keywords =
+                    HashMap::from([("int", TokenKind::Int), ("return", TokenKind::Return)]);
+                let mut identifier = ch.to_string();
+
+                while let Some(next_ch) = self.peek_char() {
+                    if !self.is_at_word_boundary() {
+                        identifier.push(self.next_char().unwrap());
+                    } else {
+                        break;
+                    }
+                }
+
+                if let Some(keyword) = keywords.get(identifier.as_str()) {
+                    keyword.clone()
+                } else {
+                    TokenKind::Identifier(identifier)
+                }
+            }
+            '(' => TokenKind::LeftParen,
+            ')' => TokenKind::RightParen,
+            '{' => TokenKind::LeftBrace,
+            '}' => TokenKind::RightBrace,
+            ';' => TokenKind::Semicolon,
+            _ => {
+                return Err(Error {
+                    message: format!("Unexpected character: '{}'", ch),
+                    span: start_position.until(&self.position),
+                });
+            }
+        };
+
+        Ok(Token {
+            kind,
+            span: start_position.until(&self.position),
+        })
+    }
+
+    fn is_at_word_boundary(&self) -> bool {
+        match self.peek_char() {
+            Some(ch) => !(ch.is_alphanumeric() || ch == '_'),
+            None => true, // End of input is a word boundary
+        }
+    }
+
+    fn skip_whitespace(&mut self) {
+        while let Some(ch) = self.peek_char() {
+            if ch.is_whitespace() {
+                self.next_char();
+            } else {
+                break;
+            }
+        }
+    }
+
+    pub fn next_char(&mut self) -> Option<char> {
+        if self.position.offset < self.input.len() {
+            let ch = self.input[self.position.offset..].chars().next()?;
+            self.position.offset += ch.len_utf8();
+
+            if ch == '\r' {
+                if let Some(next_ch) = self.input[self.position.offset..].chars().next() {
+                    if next_ch == '\n' {
+                        self.position.offset += next_ch.len_utf8();
+                    }
+                }
+                self.position.line += 1;
+                self.position.column = 1;
+            } else if ch == '\n' {
+                self.position.line += 1;
+                self.position.column = 1;
+            } else {
+                self.position.column += ch.len_utf8();
+            }
+
+            Some(ch)
+        } else {
+            None
+        }
+    }
+
+    pub fn peek_char(&self) -> Option<char> {
+        self.input[self.position.offset..].chars().next()
+    }
+}
+
+#[derive(Debug)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub span: Range<SourcePosition>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenKind {
+    Identifier(String),
+    IntConstant(i64),
+    Int,
+    Return,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Semicolon,
+    EOF,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::ops::Range;
+
+pub mod lexer;
+
+#[derive(Debug, Clone)]
+pub struct SourcePosition {
+    pub line: usize,
+    pub column: usize,
+    pub offset: usize,
+}
+
+#[derive(Debug)]
+pub struct Error {
+    pub message: String,
+    pub span: Range<SourcePosition>,
+}
+
+impl SourcePosition {
+    pub fn until(&self, other: &SourcePosition) -> Range<SourcePosition> {
+        Range {
+            start: self.clone(),
+            end: other.clone(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +29,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_lexer_simple() {
+        let input =
+            std::fs::read_to_string("tests/simple.c").expect("Failed to read test input file");
+        let mut lexer = lexer::Lexer::new(&input);
+        
+        loop {
+            match lexer.next_token() {
+                Ok(token) => {
+                    println!("{:?}", token);
+                    if token.kind == lexer::TokenKind::EOF {
+                        break;
+                    }
+                }
+                Err(e) => {
+                    panic!("Lexer error: {:?}", e);
+                }
+            }
+        }
     }
 }
